a387825bc1cd84140af494c6f1bbaa91cdb37fc5
Laura <lhutt1991@gmail.com>
2020-11-03 14:30:02 +0000

    updates and security fixes

---
+++ build/precache-manifest.dee023de19cd013a0407e5857fc4aae5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99304312bff05078707757936ddda7c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5acee249589c06a613",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2d4ef1f20d9f96251b1c",
+    "url": "/static/css/main.d76ea101.chunk.css"
+  },
+  {
+    "revision": "ba5acee249589c06a613",
+    "url": "/static/js/2.a3c3148b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3c3148b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d4ef1f20d9f96251b1c",
+    "url": "/static/js/main.7c1c8338.chunk.js"
+  },
+  {
+    "revision": "1fa64fddcb679c281499",
+    "url": "/static/js/runtime-main.3e607b21.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
