22b80f002cea3c61d6132f2eeb1c017f5cb65e10
Laura <lhutt1991@gmail.com>
2020-11-03 17:31:59 +0000

    Updates

---
+++ precache-manifest.189cbd00bcd2ff10c66ddc8638d13138.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931210cc2171296c6a3a61bbf007b1ec",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "2048597719fdf0eac2ab",
+    "url": "/robofriend/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "0d4eb35296d5a1bf46db",
+    "url": "/robofriend/static/css/main.1ba42496.chunk.css"
+  },
+  {
+    "revision": "2048597719fdf0eac2ab",
+    "url": "/robofriend/static/js/2.165b9887.chunk.js"
+  },
+  {
+    "revision": "cb4738a14a2ef86f47cb194746bb3116",
+    "url": "/robofriend/static/js/2.165b9887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4eb35296d5a1bf46db",
+    "url": "/robofriend/static/js/main.ad3a3e67.chunk.js"
+  },
+  {
+    "revision": "46429551c8d3986a48bf",
+    "url": "/robofriend/static/js/runtime-main.b3047b56.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
